bef9a9a8fe9313af9af6fb834a65f61d0c5ad225
pmadhureddy <pmadhureddy1510@gmail.com>
2024-09-30 21:13:48 +0530

    VPC Module which is reusable to create vpc

---
+++ variabales.tf
@@ -0,0 +1,81 @@
+variable "project_name" {
+    type = string
+  
+}
+
+variable "environment" {
+    type = string
+  
+}
+
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+  default = "true"
+}
+
+#Optional
+variable "common_tags" {
+  default = {}
+}
+
+variable "vpc_tags" {
+  default = {}
+}
+
+
+#Internet Gateway
+
+variable "igw_tags" {
+  default = {}
+}
+
+#Subnet
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+  validation {
+    condition     = length(var.public_subnet_cidrs) == 2
+    error_message = "Please 2 valid and public subnet CIDR's"
+  }
+}
+
+variable "public_subnet_tags" {
+  default = {}
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+  validation {
+    condition     = length(var.private_subnet_cidrs) == 2
+    error_message = "Please 2 valid and private subnet CIDR's"
+  }
+}
+
+variable "private_subnet_tags" {
+  default = {}
+}
+
+variable "database_subnet_cidrs" {
+  type = list(string)
+  validation {
+    condition     = length(var.database_subnet_cidrs) == 2
+    error_message = "Please 2 valid and database subnet CIDR's"
+  }
+}
+
+variable "database_subnet_tags" {
+  default = {}
+}
+
+variable "db_subnet_group_tags" {
+  default = {}
+  
+}
+
+variable "nat_gateways_tags" {
+  default = {}
+}
